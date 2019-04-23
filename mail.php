72e8978cc282413bfdd57cbf49b4502cc3a66bc4
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-04-23 20:46:03 +0700

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>helloPEA</title>
+</head>
+<body>
+<?php
+
+			$to="aanongnart.non@pea.co.th";
+            $subject="Email verification for PEA HR LINE bot";
+            $header .= "MIME-Version: 1.0"."\r\n";
+            $header .= 'Content-type: text/html; charset=utf-8'."\r\n";
+            $header .= 'From:กองจัดการงานทั่วไป ภาค 4 <dgop4.gad@gmail.com>'."\r\n";
+            $body .="<html></body><div><div>สวัสดีคุณpop, </div>";
+            $body .="<div style='padding-top:8px;'>กรุณายืนยันการลงทะเบียน PEA HR LINE bot</div><div style='padding-top:10px;'><a href='https://allbackoffice.000webhostapp.com/hr/activation.php?code=9998'><button>ยืนยันการลงทะเบียน</button></a></div></div>
+            </body></html>";
+            $flgSend = mail($to,$subject,$body,$header);
+	if($flgSend)
+	{
+		echo "Mail sending.";
+	}
+	else
+	{
+		echo "Mail cannot send.";
+	}
+?>
+</body>
+</html>
