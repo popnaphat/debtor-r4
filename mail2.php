1a8ec0d913404b32d874caeda2270ece46ac9933
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-04-23 22:06:30 +0700

    Update mail2.php

--- mail2.php
+++ mail2.php
@@ -2,34 +2,14 @@
 $to = "aanongnart.non@pea.co.th";
 $subject = "HTML email";
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+$message = "77777";
 
 // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
 
 // More headers
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
+$headers = "From: webmaster@example.com";
+
 
 $aa = mail($to,$subject,$message,$headers);
 if($aa){
