d7d669f0f5d980548887e6d818c45248628616e0
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-04-23 21:14:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-<html>
-<head>
-<title>helloPEA</title>
-</head>
-<body>
 <?php
 
-			$to="aanongnart.non@pea.co.th";
+		$to="aanongnart.non@pea.co.th";
             $subject="Email verification for PEA HR LINE bot";
             $header .= "MIME-Version: 1.0"."\r\n";
-            $header .= "Content-type: text/html; charset=utf-8"."\r\n";
+            $header .= "Content-type: text/html; charset=UTF-8"."\r\n";
             $header .= 'From:กองจัดการงานทั่วไป ภาค 4 <dgop4.gad@gmail.com>'."\r\n";
             $body .="<html><body><div><div>สวัสดีคุณpop, </div>";
             $body .="<div style='padding-top:8px;'>กรุณายืนยันการลงทะเบียน PEA HR LINE bot</div><div style='padding-top:10px;'><a href='https://allbackoffice.000webhostapp.com/hr/activation.php?code=9998'><button>ยืนยันการลงทะเบียน</button></a></div></div>
@@ -20,8 +15,6 @@
 	}
 	else
 	{
-		echo "Mail cannot send.";
+		echo "Mail cannot send.lll";
 	}
 ?>
-</body>
-</html>
