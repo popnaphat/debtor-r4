b2b807c0384e7a1b887244970ea1292df817d284
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-06-12 16:16:04 +0700

    Delete conn.php

--- hrt/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = new mysqli('localhost', 'id6587518_line2', '123456', 'id6587518_line2');
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-
-?>
