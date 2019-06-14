e2ff6c79ce459f6592e1c300c6a882317b7d3f61
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-06-14 21:34:38 +0700

    Update botgetuserid.php

--- hr/botgetuserid.php
+++ hr/botgetuserid.php
@@ -164,10 +164,10 @@ if($event['type'] == 'postback') {
          if($postbackstatus == 'confirm' /*AND $p6 == ""*/){
 		 	
 	$from = new SendGrid\Email(null, "HRrg4@pea.co.th");
-	$content = new SendGrid\Content("text/plain", "confirm register click: ");
+	//$content = new SendGrid\Content("text/plain", "confirm register click: ");
 	$subject = "To K.$p1 please confirm LINE bot";
 	$to = new SendGrid\Email(null, "$p3");
-	//$content = new SendGrid\Content("text/plain", "confirm register click: https://$p5");
+	$content = new SendGrid\Content("text/plain", "confirm register click: https://\"$p5\"");
 	$mail = new SendGrid\Mail($from, $subject, $to, $content);
 	$apiKey = getenv('SENDGRID_API_KEY');
 	$sg = new \SendGrid($apiKey);
