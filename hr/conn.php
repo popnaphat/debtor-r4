3ad36dfb617f20ebfc1d1ef2f51e8e40ff285c65
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-06-14 10:30:39 +0700

    Update conn.php

--- hr/conn.php
+++ hr/conn.php
@@ -6,11 +6,8 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-
-
 $conn = new mysqli($server, $username, $password, $db);
-	
-
+mysqli_query($conn, "SET NAMES utf8");
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
