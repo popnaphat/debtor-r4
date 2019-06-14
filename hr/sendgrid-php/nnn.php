1c4d4f747a7b9e70cf1163f14bd4e7b981c65017
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-06-14 20:42:33 +0700

    Update nnn.php

--- hr/sendgrid-php/nnn.php
+++ hr/sendgrid-php/nnn.php
@@ -13,9 +13,9 @@ $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
+$sg->client->mail()->send()->post($mail);
+/*$response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 echo $response->headers();
 echo $response->body();
-echo "ส่งแล้ว";
+echo "ส่งแล้ว";*/
