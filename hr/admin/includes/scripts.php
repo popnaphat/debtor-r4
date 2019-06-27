40b1755d3d50dc691773e3d1f8d4603a335aa67d
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-06-27 16:03:11 +0700

    Update scripts.php

--- hr/admin/includes/scripts.php
+++ hr/admin/includes/scripts.php
@@ -58,10 +58,18 @@
       'info'        : true,
       'autoWidth'   : false
     })
-    $('#example3').DataTable( {
+    $('#employee-grid').DataTable( {
         "processing": true,
         "serverSide": true,
-        "ajax": "server_processing.php"
+        "ajax":{
+		url :"employee-grid-data.php", // json datasource
+		type: "post",  // method  , by default get
+		error: function(){  // error handling
+				$(".employee-grid-error").html("");
+				$("#employee-grid").append('<tbody class="employee-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+				$("#employee-grid_processing").css("display","none");
+				}
+		}
     } );
   })
 </script>
