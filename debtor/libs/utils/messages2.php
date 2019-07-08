b879d4fa73708086ddcb4acaef72abc0f644e02b
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-08 15:29:00 +0700

    Create messages2.php

---
+++ debtor/libs/utils/messages2.php
@@ -0,0 +1,58 @@
+                      "wrap": true
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "spacing": "sm",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "จำนวน",
+                      "flex": 0,
+                      "size": "sm",
+                      "color": "#AAAAAA",
+                      "wrap": true
+                    },
+                    {
+                      "type": "text",
+                      "text": "'.$countemp.' ราย",
+                      "flex": 0,
+                      "size": "sm",
+                      "color": "#666666",
+                      "wrap": true
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "vertical",
+          "flex": 0,
+          "spacing": "sm",
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "คลิกดูรายละเอียด",
+                "uri": "https://southpea.herokuapp.com/debtor/majorDebt/req_office.php?REQ='.$dept_code.'"
+              },
+              "height": "sm",
+              "style": "primary"
+            },
+            {
+              "type": "spacer",
+              "size": "sm"
+            }
+          ]
+        }
+      }
+    }';
+    $result = json_decode($json);
+    return $result;
+  }
