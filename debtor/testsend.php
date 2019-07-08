878665c2fabc5182bfb326379395a148a9e8a3ee
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-08 15:44:10 +0700

    Update testsend.php

--- debtor/testsend.php
+++ debtor/testsend.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Bangkok");
   require('./majorDebt/conn.php');
   require('./libs/utils/date_thai.php');
   require('./libs/utils/date_utils.php');
-  require('./libs/utils/messages.php');
+  require('./libs/utils/messages2.php');
 
   // line access token
   $access_token = 'CGBgbM7ECUjswllXeJ6MIegVud5ulkBjM0ZU+z0GIWkXUIPAm1JC9uUAsycDJHbIuHKcHrEr8GmeS1/2eVV4E/NBiutlQHAPLJXbz58Voa9uHdK3R8/E1qN0Ox0STooKId3oiFvpRAYT3my/ZkjA8QdB04t89/1O/w1cDnyilFU=';
