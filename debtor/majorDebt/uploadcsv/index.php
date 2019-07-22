aca7aaf367721b98515d07d8349c73880e50d261
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-22 13:56:46 +0700

    Update index.php

--- debtor/majorDebt/uploadcsv/index.php
+++ debtor/majorDebt/uploadcsv/index.php
@@ -31,8 +31,8 @@ if (isset($_POST["import"])) {
     $fileName = $_FILES["file"]["tmp_name"];
     if ($_FILES["file"]["size"] > 0) {
         $file = fopen($fileName, "r");
+        uploadCSVFile($conn,$filenn);
         while (($column = fgetcsv($file, 10000, "#","#")) !== FALSE) {
-            uploadCSVFile($conn,$filenn);
             $timeupload = DateThai(date("Y-m-d"));
             $sqlInsert = "INSERT into debtor(sap_code,dept_name,cus_number,cus_name,bill_month,outstanding_debt,bail,diff,timeupload)
                    values ('" . $column[0] . "','" . $column[1] . "','" . $column[2] . "','" . $column[3] . "','" . $column[4] . "','" . $column[5] . "','" . $column[6] . "','" . $column[7] . "','" . $timeupload . "')";
