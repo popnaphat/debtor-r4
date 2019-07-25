89b9dfe0897bbb08a849681da42ab26dcb539828
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-25 23:39:00 +0700

    Update pushnum.php

--- debtor/pushnum.php
+++ debtor/pushnum.php
@@ -1,24 +1,18 @@
 <?php 
 $access_token = 'CGBgbM7ECUjswllXeJ6MIegVud5ulkBjM0ZU+z0GIWkXUIPAm1JC9uUAsycDJHbIuHKcHrEr8GmeS1/2eVV4E/NBiutlQHAPLJXbz58Voa9uHdK3R8/E1qN0Ox0STooKId3oiFvpRAYT3my/ZkjA8QdB04t89/1O/w1cDnyilFU=';
-$headers = "Authorization: Bearer $access_token";
+$headers = array('Authorization: Bearer ' . $access_token);
+      
 $url = "https://api.line.me/v2/bot/message/quota/consumption";
 //  Initiate curl
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
-print_r($result);
-// Will dump a beauty json :3
-//var_dump(json_decode($result, true));
-//Using file_get_contents
 
-//$result2 = file_get_contents($url);
-// Will dump a beauty json :3
-//var_dump(json_decode($result2, true));
+$cURL = curl_init();
+
+curl_setopt($cURL, CURLOPT_URL, $url);
+curl_setopt($cURL, CURLOPT_HTTPGET, true);
+curl_setopt($cURL, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($cURL);
+
+curl_close($cURL);
+print_r($result);
 ?>
