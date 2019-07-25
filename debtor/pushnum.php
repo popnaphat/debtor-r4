371c0c3fde564c3186e4ef8fb430180d53451222
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-25 14:27:12 +0700

    Create pushnum.php

---
+++ debtor/pushnum.php
@@ -0,0 +1,21 @@
+<?php 
+$access_token = 'CGBgbM7ECUjswllXeJ6MIegVud5ulkBjM0ZU+z0GIWkXUIPAm1JC9uUAsycDJHbIuHKcHrEr8GmeS1/2eVV4E/NBiutlQHAPLJXbz58Voa9uHdK3R8/E1qN0Ox0STooKId3oiFvpRAYT3my/ZkjA8QdB04t89/1O/w1cDnyilFU=';
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+
+  function httpGet($url)
+  {
+      $ch = curl_init();  
+   
+      curl_setopt($ch,CURLOPT_URL,$url);
+      curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch,CURLOPT_HTTPHEADER, $headers); 
+   
+      $output=curl_exec($ch);
+   
+      curl_close($ch);
+      return $output;
+  }
+   
+  echo httpGet("https://api.line.me/v2/bot/message/quota");
+?>
