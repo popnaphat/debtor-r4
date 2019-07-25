5ccb99cd1557fbb6d65c46f3f3da3ebaca79970c
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-25 23:32:17 +0700

    Update pushnum.php

--- debtor/pushnum.php
+++ debtor/pushnum.php
@@ -1,6 +1,6 @@
 <?php 
 $access_token = 'CGBgbM7ECUjswllXeJ6MIegVud5ulkBjM0ZU+z0GIWkXUIPAm1JC9uUAsycDJHbIuHKcHrEr8GmeS1/2eVV4E/NBiutlQHAPLJXbz58Voa9uHdK3R8/E1qN0Ox0STooKId3oiFvpRAYT3my/ZkjA8QdB04t89/1O/w1cDnyilFU=';
-$headers = array('Content-Type: application/json','Authorization: Bearer ' . $access_token);
+$headers = "Authorization: Bearer $access_token";
 $url = "https://api.line.me/v2/bot/message/quota/consumption";
 //  Initiate curl
 $ch = curl_init($url);
