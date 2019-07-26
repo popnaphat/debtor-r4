99a2daed1a0f49a23e3ef7ab28d9ea8befc2276c
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-07-26 07:23:18 +0700

    Update pushnum.php

--- debtor/pushnum.php
+++ debtor/pushnum.php
@@ -11,6 +11,7 @@ curl_setopt($cURL, CURLOPT_URL, $url);
 curl_setopt($cURL, CURLOPT_HTTPGET, true);
 curl_setopt($cURL, CURLOPT_HTTPHEADER, $headers);
 curl_exec($cURL);
+curl_close($cURL);
 /*$result = curl_exec($cURL);
 
 curl_close($cURL);
