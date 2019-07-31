c9af7ce134c82428e88082a695abb6a7befe87a5
PopNaphat <naphat.ana@gmail.com>
2019-07-31 23:21:16 +0700

    Update request_modal.php

--- hr/admin/includes/request_modal.php
+++ hr/admin/includes/request_modal.php
@@ -121,7 +121,7 @@
           	</div>
           	<div class="modal-footer">
             	<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i>Close</button>
-            	<button type="submit" class="btn btn-success btn-flat" name="delete"><i class="fa fa-trash"></i>Approve</button>
+            	<button type="submit" class="btn btn-success btn-flat" name="delete"><i class="fa fa-check-square-o"></i>Approve</button>
             	</form>
           	</div>
         </div>
