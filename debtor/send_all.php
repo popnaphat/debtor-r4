ce64b26138c907da1ca519fa694d4bcd3a9f81e0
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-08-02 10:30:10 +0700

    Update send_all.php

--- debtor/send_all.php
+++ debtor/send_all.php
@@ -5,8 +5,8 @@ date_default_timezone_set("Asia/Bangkok");
   require('./libs/utils/date_utils.php');
   require('./libs/utils/messages.php');
   require('./libs/utils/messages2.php');
-  require('./send_region.php');
-  require('./send_manager.php');
+  require('send_region.php');
+  require('send_manager.php');
 
   sendtomanager();
   echo "<br><br>";
