dc5b03251b7e2cf06e194862c9112b625e253f32
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-08-06 22:08:32 +0700

    Update send_all.php

--- debtor/send_all.php
+++ debtor/send_all.php
@@ -3,17 +3,16 @@ date_default_timezone_set("Asia/Bangkok");
   require('./majorDebt/conn.php');
   /*require('./libs/utils/date_thai.php');
   require('./libs/utils/date_utils.php');*/
-  require('./libs/utils/messages.php');
-  require('./libs/utils/messages2.php');
+  //require('./libs/utils/messages.php');
+  //require('./libs/utils/messages2.php');
   require('./send_region.php');
   require('./send_manager.php');
-  require('./pushnum.php');
+  //require('./pushnum.php');
 
   // sendtomanager();
   // sendtoregion();
   echo "ok200";
-  error_reporting(E_ALL);
-  ini_set('display_errors', 'on');
+  
   // $ttn = getnumsend();
   // echo $ttn;
   ?>
