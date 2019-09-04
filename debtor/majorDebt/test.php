c9b52b8b7026e49a4e0899d8a9cb317c26837562
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-09-04 14:51:21 +0700

    Update test.php

--- debtor/majorDebt/test.php
+++ debtor/majorDebt/test.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
-		<title>ลูกหนี้ค่าไฟฟ้าเอกชนรายใหญ่ค้างชำระเกินเงินประกัน</title>
+		
 	</head> 
 	<body> 
 	<?php echo "testlinefrontendframework"; ?>
