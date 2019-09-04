66d31d8782b17f1a7e2c4b668887642e3a705a9b
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-09-04 15:41:46 +0700

    Create test.php

---
+++ debtor/majorDebt/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+		<title>test get user view liff</title>
+	</head> 
+	<body> 
+	<?php echo "testlinefrontendframework"; ?>
+	</body>
+</html>
