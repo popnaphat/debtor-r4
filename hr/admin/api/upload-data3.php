bce946f9ef1c17816eec5f0c5a2cd5cfa92816a8
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-09-04 13:18:42 +0700

    Update upload-data3.php

--- hr/admin/api/upload-data3.php
+++ hr/admin/api/upload-data3.php
@@ -5,14 +5,15 @@
     function recordto_csvdebt1($conn){
         $reg = array("J","K","L");
         foreach($reg as $row){
-        $bm = "SELECT right(bill_month,7) as bm FROM debtor where right(bill_month,4) = YEAR(CURRENT_DATE)+543 and left(sap_code,1) = '$row' ORDER BY bill_month DESC LIMIT 1";
+        $bm = "SELECT right(bill_month,7) as bm, left(sap_code,1) as region FROM debtor where right(bill_month,4) = YEAR(CURRENT_DATE)+543 and left(sap_code,1) = '$row' ORDER BY bill_month DESC LIMIT 1";
         $querybm = mysqli_query($conn,$bm);
         $fetchbm = mysqli_fetch_array($querybm);
-        $mmm = substr($fetchbm['bill_month'],-7);
+        $mmm = substr($fetchbm['bm'],-7);
+        $nnn = $fetchbm['region'];
         $id = "SELECT * FROM tbl_log_csv_debt1";
         $countid = mysqli_num_rows(mysqli_query($conn,$id)) + 1; 
         $current_timestamp = DateThai(date("Y-m-d"));
-        $insert_log_file = "INSERT INTO tbl_log_csv_debt1(id,file_upload_timestamp,bill_month,region) VALUES('$countid','$current_timestamp','$mmm','$row')";
+        $insert_log_file = "INSERT INTO tbl_log_csv_debt1(id,file_upload_timestamp,bill_month,region) VALUES('$countid','$current_timestamp','$mmm','$nnn')";
         mysqli_query($conn, $insert_log_file) or trigger_error($conn->error."[$insert_log_file]");
         }
     }
