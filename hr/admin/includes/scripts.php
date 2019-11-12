2d14407fa93577b1285468f3a7e29f914a5c5775
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-11-12 15:27:41 +0700

    kpi

--- hr/admin/includes/scripts.php
+++ hr/admin/includes/scripts.php
@@ -84,6 +84,19 @@
 				}
 		}
     } );
+    $('#outdebtor-kpi-grid').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "ajax":{
+		url :"kpi_debtor_grid_data.php", // json datasource
+		type: "post",  // method  , by default get
+		error: function(){  // error handling
+				$(".outdebtor-kpi-grid-error").html("");
+				$("#outdebtor-kpi-grid").append('<tbody class="outdebtor-kpi-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+				$("#outdebtor-kpi-grid_processing").css("display","none");
+				}
+		}
+    } );
   })
 </script>
 <script>
