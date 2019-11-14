068bfa4f8aca981e0005a33ade2d328da1c3d17f
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-11-14 14:24:13 +0700

    Update botgetuserid.php

--- botgetuserid.php
+++ botgetuserid.php
@@ -186,21 +186,34 @@
                $cdb = mysqli_query($conn,$selectcdb);
                $countdeb = mysqli_num_rows($cdb);
                
-               $selectglr = "SELECT * FROM debtor where left(sap_code,1) = '$sapreg' LIMIT 1";
+               $selectglr = "SELECT * FROM tbl_log_csv_debt1 where region = '$sapreg' ORDER BY id DESC LIMIT 1";
                $glr = mysqli_query($conn,$selectglr);
                $getlastrow = mysqli_fetch_array($glr);
-               $dateupload = $getlastrow['timeupload'];
+               $dateupload = $getlastrow['file_upload_timestamp'];
    
                $selectcp = "SELECT dept_name, sap_code from debtor where left(sap_code,1) = '$sapreg' GROUP BY sap_code";
                $cp = mysqli_query($conn,$selectcp);
-               $countpea = mysqli_num_rows($cp); 
+               $countpea = mysqli_num_rows($cp);
+               ///////////////////////////////////////////////
+               $selectcdb2 = "SELECT * FROM debtor_kpi where left(sap_code,1) = '$sapreg'";
+               $cdb2 = mysqli_query($conn,$selectcdb2);
+               $countdeb2 = mysqli_num_rows($cdb2);
+               
+               $selectglr2 = "SELECT * FROM tbl_log_csv_debt2 where region = '$sapreg' ORDER BY id DESC LIMIT 1";
+               $glr2 = mysqli_query($conn,$selectglr2);
+               $getlastrow2 = mysqli_fetch_array($glr2);
+               $dateupload2 = $getlastrow2['file_upload_timestamp'];
+   
+               $selectcp2 = "SELECT dept_name, sap_code from debtor_kpi where left(sap_code,1) = '$sapreg' GROUP BY sap_code";
+               $cp2 = mysqli_query($conn,$selectcp2);
+               $countpea2 = mysqli_num_rows($cp2); 
                
                if($countdeb == 0){
                   $txtans = "คุณไม่มีเรื่องแจ้งเตือนสำหรับวันนี้";
                   $messages = [ 'type' => 'text', 'text' => $txtans];
                }
                else{
-               $messages = getBubbleMessages2($countpea, $countdeb, $dateupload, $regionname, $sapreg);
+               $messages = getBubbleMessages2($countpea, $countdeb, $dateupload, $countpea2, $countdeb2, $dateupload2, $regionname, $sapreg);
                }
                $data = [
                      'replyToken' => $replyToken,
