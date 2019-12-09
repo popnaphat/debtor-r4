bdf07d75621dcb5db8c98014924d2ede292e83a0
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-12-09 22:58:40 +0700

    h

--- debtor/overdue3/index.php
+++ debtor/overdue3/index.php
@@ -54,7 +54,7 @@
 						$a = 1;
 						while($result=mysqli_fetch_array($query)){
 							$reg = $result['region2'];
-							$dateupload = "SELECT bill_month FROM tbl_log_csv_debt4 where right(bill_month,4) = YEAR(CURRENT_DATE) and region = '$reg' ORDER BY bill_month DESC LIMIT 1";
+							$dateupload = "SELECT bill_month FROM tbl_log_csv_debt4 where right(bill_month,4) = '$curyear' and region = '$reg' ORDER BY bill_month DESC LIMIT 1";
 							$querydu = mysqli_query($conn,$dateupload);
 							$fetchdu = mysqli_fetch_array($querydu);
 							$mmm = $fetchdu['bill_month'];
