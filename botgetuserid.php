f9e018b889ba48a2f5591301fca811542884dfc9
popnaphat <40283665+popnaphat@users.noreply.github.com>
2019-12-13 13:54:36 +0700

    Update botgetuserid.php

--- botgetuserid.php
+++ botgetuserid.php
@@ -339,9 +339,9 @@
                   $cdb = mysqli_query($conn,$selectcdb);
                   $countdeb = mysqli_num_rows($cdb);
 
-                     $txtans .= "$number.".$eachhd['tblname_th']."ของ$zz มี $countdeb ราย \n";
+                     $txtans .= "\n$number.".$eachhd['tblname_th']."ของ$zz มี $countdeb ราย";
                   if($countdeb > 0){
-                     $txtans .= "คลิก>>".$eachhd['center_url']."/req_office.php?REQ=$zzz \n";
+                     $txtans .= "\nคลิก>>".$eachhd['center_url']."/req_office.php?REQ=$zzz \n";
                   }
                   $number++;
                }
