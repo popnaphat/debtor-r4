3391cf3d7be63074fcfe8111bcf7b5bb39ad84a8
popnaphat <40283665+popnaphat@users.noreply.github.com>
2020-08-14 13:56:48 +0700

    Update soaptest.php

--- soaptest.php
+++ soaptest.php
@@ -3,6 +3,10 @@ $db = parse_url(getenv("DATABASE_URL"));
 // $db["path"] = ltrim($db["path"], "/");
 // echo $db['path'];
 //print_r($db);
-$conn = pg_connect(getenv("DATABASE_URL"));
-print_r($conn);
+//$conn = pg_connect(getenv("DATABASE_URL"));
+//print_r($conn);
+$result = pg_query("SELECT * FROM peamember WHERE memberid = '251211'");
+while($row = pg_fetch_assoc($result)){
+print_r($row);
+}
 ?>
